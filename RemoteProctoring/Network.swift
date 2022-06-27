c856440cc42892da07d652a8a54d1093775628d4
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-06-27 18:03:20 +0500

    Network Manager with token and basicApolloClient

--- RemoteProctoring/Network.swift
+++ RemoteProctoring/Network.swift
@@ -7,8 +7,28 @@
 
 import Foundation
 import Apollo
+import KeychainAccess
 
 class Network {
     static let shared = Network()
-    private(set) lazy var basicApollo = ApolloClient(url: URL(string: "http://localhost:3001/graphql")!)
+    private(set) lazy var basicApollo = ApolloClient(url: URL(string: "http://127.0.0.1:3001/graphql")!)
+    var keychain = Keychain(service: "rps")
+    var token : String? {
+        set {
+            do {
+                if newValue == nil {
+                    try keychain.remove("token")
+                }
+                else {
+                    try keychain.set(newValue!, key: "token")
+                }
+            } catch {
+                print("Keychain Value not modified")
+            }
+        }
+        get {
+            return try? keychain.get("token")
+        }
+    }
 }
+    
