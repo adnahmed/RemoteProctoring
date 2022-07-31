123c0ce6152c1bc30ec97959b89db20f0a9014d7
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-08-01 02:24:59 +0500

    MainView Design Logo Fix

--- RemoteProctoring/Views/MainView.swift
+++ RemoteProctoring/Views/MainView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct MainView: View {
+    @EnvironmentObject var user: User
     @State private var search: String = ""
-    @State private var topBar: String = ""
+    @State private var topBarLabel: String = "Remote Proctoring System"
+    @State private var topBarImage: String = "logo"
     var body: some View {
+        GeometryReader { g in
         ScrollView {
             VStack {
-                TopBarView()
-                Divider()
-                Spacer()
+                    TopBarView(geometry: g,
+                               image: $topBarImage,
+                               label: $topBarLabel)
+                    Divider()
+                    Spacer()
+                }
             }
+        }
+        .background(.white)
+        .onAppear {
+            
         }
     }
 }
@@ -37,26 +47,34 @@ extension View {
     }
 }
 struct TopBarView: View {
+    var geometry: GeometryProxy
+    @Binding var image: String
+    @Binding var label: String
     var body: some View {
-        HStack {
-            Image("logo")
-                .resizable()
-                .interpolation(/*@START_MENU_TOKEN@*/.high/*@END_MENU_TOKEN@*/)
-                .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-                .aspectRatio(contentMode: ContentMode.fit)
-            Text("Remote Proctoring System")
-                .padding(.top)
-                .font(Font.system(size: 40,
-                                  weight: .thin,
-                                  design: .rounded))
-                .compatibleThinFont()
-                .onTapGesture {
-                    
-                }
-                .onAppear {
-                    
-                }
-        }
+            HStack {
+                Spacer()
+                Image(image)
+                    .resizable()
+                    .interpolation(/*@START_MENU_TOKEN@*/.high/*@END_MENU_TOKEN@*/)
+                    .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geometry.size.width * 0.20,
+                           height: geometry.size.height * 0.10,
+                           alignment: .trailing)
+                Text(label)
+                    .font(Font.system(size: 40,
+                                      weight: .thin,
+                                      design: .rounded))
+                    .compatibleThinFont()
+                    .onTapGesture {
+                        
+                    }
+                    .onAppear {
+                        
+                    }
+                Spacer()
+            }
+            .padding(.top)
     }
 }
 
