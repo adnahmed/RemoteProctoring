a95ff7b1efe03389b316af007b58c0fd6209682c
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-08-16 00:03:11 +0500

    NOthing is working, fuck you xcode 14

---
+++ RemoteProctoring/Views/Supporting/ExamRoomViewSupporting.swift
@@ -0,0 +1,20 @@
+//
+//  ExamRoomViewSupporting.swift
+//  RemoteProctoring
+//
+//  Created by Adnan Ahmed Khan on 15/08/2022.
+//
+
+import SwiftUI
+
+struct ExamRoomViewSupporting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExamRoomViewSupporting_Previews: PreviewProvider {
+    static var previews: some View {
+        ExamRoomViewSupporting()
+    }
+}
